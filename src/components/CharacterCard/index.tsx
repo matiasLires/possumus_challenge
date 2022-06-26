dd0bf279a941fc5adf8b65d1887ca214d8d5e0ca
matiasLires01 <lires.matiash@gmail.com>
2022-06-26 15:27:43 -0300

    Refactorizacion de componentes

--- src/components/CharacterCard/index.tsx
+++ src/components/CharacterCard/index.tsx
@@ -1,21 +1,21 @@
-import { Character } from '../../types';
 import { useNavigate } from 'react-router-dom';
-interface Props {
-	character: Character;
+interface CharacterCardProps {
+	name: String;
+	url: String;
 }
 
-export const CharacterCard = ({ character }: Props) => {
+export const CharacterCard = ({ name, url }: CharacterCardProps) => {
 	const navigate = useNavigate();
 
 	const handleDetails = () =>
-		navigate(`character/${character.name}/details`, { state: character.url });
+		navigate(`character/${name}/details`, { state: url });
 
 	return (
 		<div
 			onClick={handleDetails}
 			className='bg-white flex justify-center items-center w-80 m-2 p-2 rounded-lg border-2 border-gray-50 shadow-lg cursor-pointer transform scale-95 hover:scale-105 transition-colors duration-75'
 		>
-			<div>{character.name}</div>
+			<div>{name}</div>
 		</div>
 	);
 };
